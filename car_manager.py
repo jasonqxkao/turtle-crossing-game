a633a523d1ff1464e095b6755f91182b85a4f2a8
jasonqxkao <142387962+jasonqxkao@users.noreply.github.com>
2023-09-21 20:23:42 -0700

    Update car_manager.py

--- car_manager.py
+++ car_manager.py
@@ -1,1 +1,35 @@
+import random
+from turtle import Turtle
+COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
+STARTING_MOVE_DISTANCE = 5
+MOVE_INCREMENT = 10
+
+
+class CarManager:
+
+    def __init__(self):
+        self.cars = []
+        self.number = 6
+        self.car_speed = STARTING_MOVE_DISTANCE
+
+    def create_cars(self):
+        if self.number % 6 == 0:
+            new_car = Turtle()
+            new_car.penup()
+            new_car.shape("square")
+            new_car.color(random.choice(COLORS))
+            new_car.shapesize(stretch_wid=1, stretch_len=2)
+            new_car.setheading(180)
+            new_car.goto(300, random.randrange(-250, 250, 20))
+            self.cars.append(new_car)
+        self.number += 1
+
+    def move_forward(self):
+        for car in self.cars:
+            car.forward(self.car_speed)
+
+    def level_up(self):
+        self.car_speed += MOVE_INCREMENT
+
+
 
